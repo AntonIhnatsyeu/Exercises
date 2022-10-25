8a213a032ed250e85885ab602154fb1b1a171dcb
AntonIhnatsyeu <ihnatsyeu1999@icloud.com>
2022-10-26 01:52:17 +0300

    1
    1

--- 1.1.20/Method.java
+++ 1.1.20/Method.java
@@ -1,17 +1,21 @@
 import java.lang.Math;
+
 public class Method {
     static int N = 4;
     static double fact;
+
     public static double factorial(int N) {
         double result = 1;
         if (N == 1 || N == 0) {
             return result;
-        }
+        } else {
             result = N * factorial(N - 1);
             return result;
+        }
     }
-    public static double logarithm(){
-         fact = Math.log(factorial(N));
-         return fact;
+
+    public static double logarithm() {
+        fact = Math.log(factorial(N));
+        return fact;
     }
 }
